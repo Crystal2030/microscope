03de18be6bf46373e8d1134a5d7b00db98922b05
crystal2030 <867389458@qq.com>
2015-08-11 15:54:37 +0800

    Added test.

--- packages/errors/package.js
+++ packages/errors/package.js
@@ -19,7 +19,7 @@ Package.onUse(function(api) {
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('tmeasday:errors');
-  api.addFiles('errors-tests.js');
+  api.use(['tinytest', 'test-helpers'], 'client');
+  api.use('tmeasday:errors', 'client');
+  api.addFiles('errors-tests.js', 'client');
 });
